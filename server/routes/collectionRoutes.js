d6560d4441e5caeac84db3608acd938925af9d01
FinnoKL <145428166+FinnoKL@users.noreply.github.com>
2025-03-25 14:56:47 +0300

    Delete server directory

--- server/routes/collectionRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { addToCollection } = require('../controllers/collectionController');
-const router = express.Router();
-
-// Добавить рекомендацию в коллекцию
-router.post('/add', addToCollection);
-
-module.exports = router;
