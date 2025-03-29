73c897f7010bcf20f80cbc035e8e2e3bb7ab289b
Nikolasina Victoria <vnikolasina00@gmail.com>
2025-03-29 19:48:14 +0300

    update

--- server/db.js
+++ server/db.js
@@ -12,23 +12,4 @@ const pool = mysql.createPool({
         rejectUnauthorized: true
     }
 });
-connection.connect((err) => {
-    if (err) {
-        console.error('Ошибка подключения к базе данных:', err);
-        return;
-    }
-    console.log('Успешно подключено к базе данных');
-});
-
-// Пример запроса к базе данных
-connection.query('SELECT * FROM users', (err, results) => {
-    if (err) {
-        console.error('Ошибка при выполнении запроса:', err);
-        return;
-    }
-    console.log('Результаты запроса:', results);
-});
-
-// Закрытие соединения после работы
-connection.end();
 module.exports = pool;
